2cd158026d142ad8af6b53f6d244f3db612158d6
Ferris Nicolaisen <t.nicolaisen@iplabs.de>
2010-06-16 15:40:08 +0200

    Added some more examples.

--- src/test/java/com/tfnico/examples/guava/BaseTest.java
+++ src/test/java/com/tfnico/examples/guava/BaseTest.java
@@ -8,11 +8,14 @@ import static org.junit.Assert.assertTrue;
 
 import java.nio.charset.Charset;
 import java.util.Arrays;
+import java.util.concurrent.Future;
 
 import org.junit.Test;
 
+import com.google.common.base.CaseFormat;
 import com.google.common.base.Charsets;
 import com.google.common.base.Defaults;
+import com.google.common.base.Equivalence;
 import com.google.common.base.Equivalences;
 import com.google.common.base.Function;
 import com.google.common.base.Functions;
@@ -21,20 +24,20 @@ import com.google.common.base.Objects;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
+import com.google.common.base.Service;
 import com.google.common.base.Splitter;
 import com.google.common.base.Strings;
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
+import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
 
-
-/**
- * Unit test for simple App.
- */
-public class GuavaTest
+public class BaseTest
 {
 
     @Test
-    public void guavaBase()
+    public void charSetsAndDefaults()
     {
         // Here's some charsets
         Charset utf8 = Charsets.UTF_8;
@@ -49,9 +52,9 @@ public class GuavaTest
     @Test
     public void equalityAndIdentity()
     {
+    	//These could be useful for building equals methods
         assertFalse(Equivalences.equals().equivalent("you", null));
-        assertTrue(Equivalences.equals().equivalent("hey", "hey"));
-
+        assertTrue(Equivalences.identity().equivalent("hey", "hey"));
     }
 
     @Test
@@ -179,6 +182,73 @@ public class GuavaTest
         assertEquals(2, ImmutableSet.copyOf(filtered).size());
 
     }
+    
+    @Test
+	public void someSuppliers()
+    {
+    	//Imagine we have Supploer that produces ingredients
+    	IngredientsFactory ingredientsFactory = new IngredientsFactory();
+			
+		//A function 'bake' that transforms ingredients into cakes
+		bake();
+		
+		//Then it's pretty easy to get a Factory that bakes cakes :)
+		Supplier<Cake> cakeFactory = Suppliers.compose(bake(), ingredientsFactory );
+		cakeFactory.get();
+		cakeFactory.get();
+		cakeFactory.get();
+		
+		assertEquals(3, ingredientsFactory.getNumberOfIngredientsUsed());
+		
+	}
+    
+    @Test
+    public void someThrowables() 
+    {
+    	try
+    	{
+    		try{
+    			Integer.parseInt("abc");
+    		}
+    		catch(RuntimeException e){
+    			if(e instanceof ClassCastException) throw e; //old-style
+    			Throwables.propagateIfInstanceOf(e, NumberFormatException.class); //the same
+    			Throwables.propagateIfPossible(e); // Propagates if it is Error or RuntimeException
+    			try {
+					Throwables.throwCause(e, true);
+				} catch (Exception e1) {
+					Throwables.propagate(e1); //Wraps if its a checked exception, or lets it flow if not
+				}
+    		}
+    	}
+    	catch(RuntimeException e){
+    		Throwables.getCausalChain(e);
+    		Throwables.getRootCause(e);
+    		Throwables.getStackTraceAsString(e);
+    	}
+    }
+    
+    
+    @Test
+	public void someEnums()
+    {
+    	assertEquals("UNDER_DOG",CaseFormat.LOWER_CAMEL.to(CaseFormat.UPPER_UNDERSCORE, "underDog"));
+    	
+    	//Controlling services
+    	Service service = new FunkyService();
+    	service.start();
+    	assertEquals(Service.State.RUNNING, service.state());
+	}
+    
+
+	private Function<Ingredients, Cake> bake() {
+		return new Function<Ingredients, Cake>() {
+			public Cake apply(Ingredients ingredients) {
+				return new Cake(ingredients);
+			}
+		};
+	}
+    
 
 }
 
@@ -194,9 +264,26 @@ class Customer
         this.name = name;
     }
     
-    
-
     @Override
+	public boolean equals(Object obj) {
+    	if(!(obj instanceof Customer)) return false;
+    	Customer that = (Customer)obj;
+    	
+    	Equivalence<Object> nae = Equivalences.nullAwareEquals();
+		return nae.equivalent(name, that.getName())
+    		&& nae.equivalent(id, that.getId());
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hashCode(id, name);
+	}
+
+
+
+	@Override
     public String toString()
     {
         return name + " (id "+id+")";
@@ -223,4 +310,64 @@ class Customer
     {
         return name;
     }
+}
+
+class Ingredients{
+	
+}
+
+class Cake {
+	Cake(Ingredients ingredients) {
+		
+	}
+}
+
+class IngredientsFactory implements Supplier<Ingredients>{
+
+	private int counter;
+
+	public Ingredients get() {
+		counter++;
+		return new Ingredients();
+	}
+	
+	int getNumberOfIngredientsUsed(){
+		return counter;
+	}
+	
+}
+
+class FunkyService implements Service{
+
+	private State state;
+
+	public boolean isRunning() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Future<State> start() {
+		this.state = Service.State.RUNNING;
+		return null;
+	}
+
+	public State startAndWait() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public State state() {
+		return state;
+	}
+
+	public Future<State> stop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public State stopAndWait() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
